6fec8efc5dc764a7e5160fefcb37f3df6813884d
Steve <steveng.1988@gmail.com>
2014-10-21 23:24:36 +0800

    updated gh-pages with v0.2.0

--- bower_components/angular-notys/README.md
+++
@@ -1,2 +0,0 @@
-# Noty inspired notification for AngularJS 
-
--- scripts/controllers/showNoty.js
+++ scripts/controllers/showNoty.js
@@ -9,7 +9,7 @@ angular.module('staticApp').controller('ShowNotyCtrl',
   		noty.showNoty({
 		  text: "Deleted an email",
 		  ttl: 1000000, //time to live in miliseconds
-		  type: "success", //success, warning 
+		  type: "success", //success, warning, default, gmail 
 		  options: ['Undo', 'Dismiss'], 
 		  optionsCallBack:  function callback(optionClicked, optionIndexClicked) {
 		    //handling code for options clicked
